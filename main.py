0e87440f9c69210805c404094f560943dba39d34
semsotsemm <antipovalexey476@gmail.com>
2025-06-18 21:25:47 +0300

    Добавленно деление на ноль, кнопки <- и C

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import flet as ft
 
 def main(page: ft.Page):
     def get_info(e, i):
-        if( result.value != '0'):
+        if( result.value != "0"):
             result.value += numbers[i].text
         else:
             result.value = numbers[i].text
@@ -15,16 +15,29 @@ def main(page: ft.Page):
 
     def get_result(e):
         try:
-            result.value = str(eval(result.value))  # Вычисляем выражение
+            expression = result.value
+            if "/0" in expression:
+                if "-" in expression:
+                    result.value = "-∞"
+                else:
+                    result.value = "∞"
+            else:
+                result.value = str(eval(expression))
         except Exception:
             result.value = "Ошибка"
         page.update()
     
+    def clear_strok(e, i):
+        if(i == 4):
+            result.value = result.value[:-1] if result.value != "0" else "0"
+        else:
+            result.value = "0"
+        page.update()
+
     page.title = "Калькулятор"
     page.theme_mode = "dark"
-    current_operation = '0'
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    result = ft.TextField(value="0", width = 100, text_align = ft.TextAlign.CENTER)
+    result = ft.TextField(value="0", width = 300, text_align = ft.TextAlign.CENTER, on_submit=get_result)
     numbers = [
         ft.ElevatedButton("1", on_click=lambda e: get_info(e, 0)),
         ft.ElevatedButton("2", on_click=lambda e: get_info(e, 1)),
@@ -42,15 +55,18 @@ def main(page: ft.Page):
            ft.ElevatedButton("-", on_click=lambda e: get_operation(e, 1)),
            ft.ElevatedButton("*", on_click=lambda e: get_operation(e, 2)),
            ft.ElevatedButton("/", on_click=lambda e: get_operation(e, 3)),
+           ft.ElevatedButton("<-", on_click=lambda e: clear_strok(e, 4)),
+           ft.ElevatedButton("C", on_click=lambda e: clear_strok(e, 5)),
            ft.ElevatedButton("=", on_click=lambda e: get_result(e))
     ]
+
 
     page.add(
         ft.Row([result],alignment=ft.MainAxisAlignment.CENTER),
         ft.Row([numbers[0],numbers[1],numbers[2],operations[0]],alignment=ft.MainAxisAlignment.CENTER),
         ft.Row([numbers[3],numbers[4],numbers[5],operations[1]],alignment=ft.MainAxisAlignment.CENTER),
         ft.Row([numbers[6],numbers[7],numbers[8],operations[2]],alignment=ft.MainAxisAlignment.CENTER),
-        ft.Row([numbers[9],operations[3],operations[4]],alignment=ft.MainAxisAlignment.CENTER)
+        ft.Row([numbers[9],operations[3],operations[4],operations[5],operations[6]],alignment=ft.MainAxisAlignment.CENTER)
     )
 
 ft.app(target=main #,view= ft.AppView.WEB_BROWSER)
