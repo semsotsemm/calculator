62e31a33013337d66097db375df7fc4ee0cf0abc
semsotsemm <antipovalexey476@gmail.com>
2025-06-19 22:58:53 +0300

    Обновлен визуал добаылены кнопки процента смена знака умножения исправленны ошибки

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import flet as ft
 
 def main(page: ft.Page):
+    ##Функции
     def get_info(e, i):
         if( result.value != "0"):
             result.value += numbers[i].text
@@ -9,8 +10,10 @@ def main(page: ft.Page):
         page.update()
 
     def get_operation(e, i):
-        current_operation = operations[i]
-        result.value += operations[i].text
+        if i == 9:
+            result.value = str(-1 * int(result.value))
+        else:
+            result.value += operation_symbols[i]
         page.update()
 
     def get_result(e):
@@ -21,6 +24,9 @@ def main(page: ft.Page):
                     result.value = "-∞"
                 else:
                     result.value = "∞"
+            elif "%" in expression:
+                expression = expression.replace("%", "/100")
+                result.value = str(eval(expression))
             else:
                 result.value = str(eval(expression))
         except Exception:
@@ -28,46 +34,83 @@ def main(page: ft.Page):
         page.update()
     
     def clear_strok(e, i):
-        if(i == 4):
+        if(i == 6):
             result.value = result.value[:-1] if result.value != "0" else "0"
         else:
             result.value = "0"
         page.update()
-
-    page.title = "Калькулятор"
-    page.theme_mode = "dark"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    result = ft.TextField(value="0", width = 300, text_align = ft.TextAlign.CENTER, on_submit=get_result)
-    numbers = [
-        ft.ElevatedButton("1", on_click=lambda e: get_info(e, 0)),
-        ft.ElevatedButton("2", on_click=lambda e: get_info(e, 1)),
-        ft.ElevatedButton("3", on_click=lambda e: get_info(e, 2)),
-        ft.ElevatedButton("4", on_click=lambda e: get_info(e, 3)),
-        ft.ElevatedButton("5", on_click=lambda e: get_info(e, 4)),
-        ft.ElevatedButton("6", on_click=lambda e: get_info(e, 5)),
-        ft.ElevatedButton("7", on_click=lambda e: get_info(e, 6)),
-        ft.ElevatedButton("8", on_click=lambda e: get_info(e, 7)),
-        ft.ElevatedButton("9", on_click=lambda e: get_info(e, 8)),
-        ft.ElevatedButton("0", on_click=lambda e: get_info(e, 9))
-    ]
-    operations = [
-           ft.ElevatedButton("+", on_click=lambda e: get_operation(e, 0)),
-           ft.ElevatedButton("-", on_click=lambda e: get_operation(e, 1)),
-           ft.ElevatedButton("*", on_click=lambda e: get_operation(e, 2)),
-           ft.ElevatedButton("/", on_click=lambda e: get_operation(e, 3)),
-           ft.ElevatedButton("<-", on_click=lambda e: clear_strok(e, 4)),
-           ft.ElevatedButton("C", on_click=lambda e: clear_strok(e, 5)),
-           ft.ElevatedButton("=", on_click=lambda e: get_result(e))
-    ]
-
-
-    page.add(
-        ft.Row([result],alignment=ft.MainAxisAlignment.CENTER),
-        ft.Row([numbers[0],numbers[1],numbers[2],operations[0]],alignment=ft.MainAxisAlignment.CENTER),
-        ft.Row([numbers[3],numbers[4],numbers[5],operations[1]],alignment=ft.MainAxisAlignment.CENTER),
-        ft.Row([numbers[6],numbers[7],numbers[8],operations[2]],alignment=ft.MainAxisAlignment.CENTER),
-        ft.Row([numbers[9],operations[3],operations[4],operations[5],operations[6]],alignment=ft.MainAxisAlignment.CENTER)
+    
+    #Параметры цифр
+    numbers_styles = ft.ButtonStyle(
+        bgcolor=ft.Colors.with_opacity(1, "#1F1F1F"),
+        color=ft.Colors.WHITE,
+        text_style=ft.TextStyle(weight=ft.FontWeight.W_900,font_family="Courier New")
     )
 
-ft.app(target=main #,view= ft.AppView.WEB_BROWSER)
-)
+    numbers = [
+        ft.ElevatedButton("1", on_click=lambda e: get_info(e, 0), style=numbers_styles),
+        ft.ElevatedButton("2", on_click=lambda e: get_info(e, 1), style=numbers_styles),
+        ft.ElevatedButton("3", on_click=lambda e: get_info(e, 2), style=numbers_styles),
+        ft.ElevatedButton("4", on_click=lambda e: get_info(e, 3), style=numbers_styles),
+        ft.ElevatedButton("5", on_click=lambda e: get_info(e, 4), style=numbers_styles),
+        ft.ElevatedButton("6", on_click=lambda e: get_info(e, 5), style=numbers_styles),
+        ft.ElevatedButton("7", on_click=lambda e: get_info(e, 6), style=numbers_styles),
+        ft.ElevatedButton("8", on_click=lambda e: get_info(e, 7), style=numbers_styles),
+        ft.ElevatedButton("9", on_click=lambda e: get_info(e, 8), style=numbers_styles),
+        ft.ElevatedButton("0", on_click=lambda e: get_info(e, 9), style=numbers_styles)
+    ]
+    
+    #Стили вспомогающих кнопок
+    management_styles = ft.ButtonStyle(
+        bgcolor=ft.Colors.with_opacity(1, "#4D4D4D"),
+        color=ft.Colors.WHITE,
+        text_style=ft.TextStyle(
+            weight=ft.FontWeight.W_900,
+            font_family="Courier New"
+            )
+    )
+
+    #Параметры операций
+    operations_styles = ft.ButtonStyle(
+        bgcolor=ft.Colors.with_opacity(1, "#FFC400"),
+        color=ft.Colors.WHITE,
+        text_style=ft.TextStyle(
+            weight=ft.FontWeight.W_900,
+            font_family="Courier New"
+            )
+    )
+
+    operation_symbols = ["+", "-", "*", "/", "%", "."]
+
+    operations = [
+           ft.ElevatedButton("+", on_click=lambda e: get_operation(e, 0), style=operations_styles),
+           ft.ElevatedButton("-", on_click=lambda e: get_operation(e, 1), style=operations_styles),
+           ft.ElevatedButton("×", on_click=lambda e: get_operation(e, 2), style=operations_styles),
+           ft.ElevatedButton("/", on_click=lambda e: get_operation(e, 3), style=operations_styles),
+           ft.ElevatedButton("<-", on_click=lambda e: clear_strok(e,6), style=management_styles),
+           ft.ElevatedButton("C", on_click=lambda e: clear_strok(e,7), style=management_styles),
+           ft.ElevatedButton("=", on_click=lambda e: get_result(e), style=operations_styles),
+           ft.ElevatedButton("%", on_click=lambda e: get_operation(e, 4), style=operations_styles),
+           ft.ElevatedButton("+/-", on_click=lambda e: get_operation(e,9), style=management_styles),
+           ft.ElevatedButton(".", on_click=lambda e: get_operation(e, 5), style=numbers_styles)
+    ]
+    
+    ##Базовые настройки
+    page.title = "Калькулятор"
+    page.theme_mode = "dark"
+    page.bgcolor = ft.Colors.BLACK
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    result = ft.TextField(value="0", width = 250, text_align = ft.TextAlign.CENTER, on_submit=get_result)
+
+    ##Добавление кнопок на страницу
+    page.add(
+        ft.Row([result],alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([operations[4],operations[8],operations[5],operations[3]],alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([numbers[0],numbers[1],numbers[2],operations[2]],alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([numbers[3],numbers[4],numbers[5],operations[1]],alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([numbers[6],numbers[7],numbers[8],operations[0]],alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([operations[9],numbers[9],operations[7],operations[6]],alignment=ft.MainAxisAlignment.CENTER),
+    )
+    result.focus()
+
+ft.app(target=main)
